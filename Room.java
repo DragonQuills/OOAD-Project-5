806c3e6d363d32ad2bb1aa0a2c9df0611764cfc3
Kayden <kayden.adams@colorado.edu>
2020-04-07 14:31:47 -0600

    Adds header for room

---
+++ Room.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList; //Referenced https://www.w3schools.com/java/java_arraylist.asp
+
+public class Room{
+// attributes
+public:
+  String name;
+private:
+  ArrayList<PlantPot> plants;
+  ArrayList<WaterReservoir> reservoirs;
+  TempuratureSensor temp_sensor;
+  int lowest_temp;
+  int highest_temp;
+  PlantFactory plant_factory;
+  ArrayList<App> observers;
+
+//methods
+  private void manage_room(){}
+  private void notify_observers(){}
+
+  public ArrayList<PlantPot> get_plants(){return plants;}
+  public void add_plant(Plant p){}
+  public void remove_plant(String name){}
+  public void rename_plant(String old_name, String new_name){}
+  public void add_res(WaterReservoir res){}
+  public void remove_res(String name){}
+  public void rename_res(String name){}
+  public void add_observer(App app){}
+  public void remove_observer(App app){}
+  public void update(){}
+}
