f31c55b510f4549146716a577510320f0de16e77
Kayden <kayden.adams@colorado.edu>
2020-04-09 15:20:47 -0600

    Plant pot has a reference to its water reservoir

--- PlantPot.java
+++ PlantPot.java
@@ -4,6 +4,7 @@ class PlantPot{
   public String plant_type;
   private Light light;
   private Hose hose;
+  private WaterReservoir res;
   private Timer timer;
   private WaterSensor water_sensor;
   private int desired_soil_humidity; // this determines how much the plant will be watered
@@ -14,7 +15,7 @@ class PlantPot{
   public void set_amount_to_watter(){}
   public void set_light_hours(){} //this doesn't change any of Plant's variables, it effects the Timer.
   public void check_water(){} //this checks the water sensor and then tells it to water the plant if the water level is too low
-
+  public void set_water_reservoir(WaterReservoir res){}
 //constructor
-  public PlantPot(String name, String plant_type, int desired_soil_humidity, int min_soil_humidity){}
+  public PlantPot(String name, String plant_type){}
 }
