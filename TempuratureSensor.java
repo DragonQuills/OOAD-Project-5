1920057eae977a0ab40d181d21451388f9079d95
Kayden <kayden.adams@colorado.edu>
2020-04-09 15:04:41 -0600

    Adds tempuraturesensor

---
+++ TempuratureSensor.java
@@ -0,0 +1,10 @@
+class TempuratureSensor{
+// attributes
+  private Command heat_room;
+  private Command cool_room;
+  private Command hold_temp;
+// methods
+  public float take_reading(){}
+// constructor
+  public TempuratureSensor(Command heat, Command cool, Command hold){}
+}
