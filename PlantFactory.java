fab866d647b6e40ffac6726203112280a882dddd
Kayden <kayden.adams@colorado.edu>
2020-04-09 15:13:02 -0600

    Adds plant factory

---
+++ PlantFactory.java
@@ -0,0 +1,8 @@
+class PlantFactory{
+// methods
+  public Plant get_plant(String name, String type){}
+  private Plant query_api(String type){}
+  public boolean conditions_ok_for_plant(Plant p, Room r){}
+// constructor
+  public PlantFactory(){}
+}
