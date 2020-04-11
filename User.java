4af37adfad2be097bb05282f57a9fc3ae5a08aa3
Kayden <kayden.adams@colorado.edu>
2020-04-11 17:11:38 -0600

    Adds user class

---
+++ User.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList; //Referenced https://www.w3schools.com/java/java_arraylist.asp
+
+
+class User{
+// attributes
+  private ArrayList<Room> rooms;
+// methods
+  public String status_report(){ return "";}
+  public void update(){}
+  public void add_room(){}
+  public void remove_room(String name){}
+  public void rename_room(String old_name, String new_name){}
+
+  private void update_db(){}
+}
