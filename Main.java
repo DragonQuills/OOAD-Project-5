00e69b8fe03c8012da37ea680362a99f9429b88a
Ann Marie Mahon <annmarie.mahon@colorado.edu>
2020-04-22 06:44:43 -0600

    Add registration functionality

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 import java.io.*; //reference: https://www.tutorialspoint.com/java/java_files_io.htm
+import java.nio.file.*;
 
 public class Main {
 
@@ -58,11 +59,57 @@ public class Main {
         scanner.close();
     }
 
-    private static void registerUser(){
+    private static boolean registerUser(){
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter a username: ");
         String username = scanner.next();
         System.out.println("Enter a password: ");
         String password = scanner.next();
+        File usersFile = new File("./storage/users.csv");
+        //Ref: https://tutoref.com/how-to-read-and-write-files-in-java-8/
+        int maxId;
+        try{
+            FileInputStream f = new FileInputStream(usersFile);
+            BufferedReader br = new BufferedReader(new InputStreamReader(f));
+            String line;
+            maxId = 0;
+            boolean firstLine = true;
+            while((line = br.readLine()) != null){
+                String[] splitLine = line.split(",");
+                if(firstLine){
+                    firstLine = false;
+                    continue;
+                }
+                if(splitLine[1].equals(username)){
+                    System.out.println("Username already registered");
+                    return false;
+                }
+                if(Integer.parseInt(splitLine[0]) > maxId){
+                    maxId = Integer.parseInt(splitLine[0]);
+                }
+            }
+            br.close();        
+        }
+        catch(FileNotFoundException e){
+            e.printStackTrace();
+            return false;
+        }     
+        catch(IOException e){
+            e.printStackTrace();
+            return false;
+        }   
+        //If the code has reached this point that username is not registered
+        try{
+            FileWriter fw = new FileWriter(usersFile, true);
+            BufferedWriter bw = new BufferedWriter(fw);
+            PrintWriter pw = new PrintWriter(bw);
+            String line = (maxId+1)+","+username+","+password;
+            pw.println(line);
+            pw.close();
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+        return true;
     }
 }
