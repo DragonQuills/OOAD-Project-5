da240fe119bed4ac53d6649f3c12c5f93092c9df
Ann Marie Mahon <annmarie.mahon@colorado.edu>
2020-04-22 09:43:20 -0600

    Move functions to own class

--- Main.java
+++ Main.java
@@ -1,6 +1,4 @@
 import java.util.Scanner;
-import java.io.*; //reference: https://www.tutorialspoint.com/java/java_files_io.htm
-import java.nio.file.*;
 import java.util.ArrayList;
 
 public class Main {
@@ -8,6 +6,7 @@ public class Main {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in); //For terminal input, reference: https://alvinalexander.com/java/edu/pj/pj010005/
         boolean validInput = false;
+        StorageHandler storage = StorageHandler.getInstance();
         int userId = -1;
         while(!validInput){
             System.out.println("Select an option:");
@@ -16,14 +15,14 @@ public class Main {
             int intInput = scanner.nextInt();
             if(intInput == 1){
                 while(userId == -1){
-                    userId = loginUser();
+                    userId = storage.loginUser();
                 }
                 validInput = true;
             }
             else if(intInput == 2){
                 //Redirect to register
                 while(userId == -1){
-                    userId = registerUser();
+                    userId = storage.registerUser();
                 }
                 validInput = true;
             }
@@ -51,7 +50,7 @@ public class Main {
                 int capacity = scanner.nextInt();
                 System.out.println("Warning level: ");
                 int warning = scanner.nextInt();
-                createReservoir(userId, reservoirName, capacity, warning);
+                storage.createReservoir(userId, reservoirName, capacity, warning);
             }
             else if(intInput == 3){
                 //Add plant
@@ -69,172 +68,5 @@ public class Main {
 
         }
         scanner.close();
-    }
-
-    private static int registerUser(){
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter a username: ");
-        String username = scanner.next();
-        System.out.println("Enter a password: ");
-        String password = scanner.next();
-        File usersFile = new File("./storage/users.csv");
-        //Ref: https://tutoref.com/how-to-read-and-write-files-in-java-8/
-        int maxId;
-        try{
-            FileInputStream f = new FileInputStream(usersFile);
-            BufferedReader br = new BufferedReader(new InputStreamReader(f));
-            String line;
-            maxId = 0;
-            boolean firstLine = true;
-            while((line = br.readLine()) != null){
-                String[] splitLine = line.split(",");
-                if(firstLine){
-                    firstLine = false;
-                    continue;
-                }
-                if(splitLine[1].equals(username)){
-                    System.out.println("Username already registered");
-                    return -1;
-                }
-                if(Integer.parseInt(splitLine[0]) > maxId){
-                    maxId = Integer.parseInt(splitLine[0]);
-                }
-            }
-            br.close();        
-        }
-        catch(FileNotFoundException e){
-            e.printStackTrace();
-            return -1;
-        }     
-        catch(IOException e){
-            e.printStackTrace();
-            return -1;
-        }   
-        //If the code has reached this point that username is not registered
-        try{
-            FileWriter fw = new FileWriter(usersFile, true);
-            BufferedWriter bw = new BufferedWriter(fw);
-            PrintWriter pw = new PrintWriter(bw);
-            String line = (maxId+1)+","+username+","+password;
-            pw.println(line);
-            pw.close();
-        }
-        catch(IOException e){
-            e.printStackTrace();
-            return -1;
-        }
-        return maxId+1;
-    }
-
-    private static int loginUser(){
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter a username: ");
-        String username = scanner.next();
-        System.out.println("Enter a password: ");
-        String password = scanner.next();
-        File usersFile = new File("./storage/users.csv");
-        try{
-            FileInputStream f = new FileInputStream(usersFile);
-            BufferedReader br = new BufferedReader(new InputStreamReader(f));
-            String line;
-            boolean firstLine = true;
-            while((line = br.readLine()) != null){
-                String[] splitLine = line.split(",");
-                if(firstLine){
-                    firstLine = false;
-                    continue;
-                }
-                if(splitLine[1].equals(username) && splitLine[2].equals(password)){
-                    br.close(); 
-                    return Integer.parseInt(splitLine[0]);
-                }
-            }
-            br.close();        
-        }
-        catch(FileNotFoundException e){
-            e.printStackTrace();
-            return -1;
-        }     
-        catch(IOException e){
-            e.printStackTrace();
-            return -1;
-        }
-        // This reaching this point means that username/password combo was not found
-        System.out.println("Username does not exist or incorrect password was entered");
-        return -1;
-    }
-
-    private static WaterReservoir createReservoir(int userId, String name, int capacity, int warning){
-        File reservoirsFile = new File("./storage/reservoirs.csv");
-        int maxId = 0;
-        try{
-            FileInputStream f = new FileInputStream(reservoirsFile);
-            BufferedReader br = new BufferedReader(new InputStreamReader(f));
-            String line;
-            boolean firstLine = true;
-            while((line = br.readLine()) != null){
-                String[] splitLine = line.split(",");
-                if(firstLine){
-                    firstLine = false;
-                    continue;
-                }
-                if(Integer.parseInt(splitLine[0]) > maxId){
-                    maxId = Integer.parseInt(splitLine[0]);
-                }
-            }
-            br.close();        
-        }
-        catch(FileNotFoundException e){
-            e.printStackTrace();
-        }     
-        catch(IOException e){
-            e.printStackTrace();
-        }
-
-        try{
-            FileWriter fw = new FileWriter(reservoirsFile, true);
-            BufferedWriter bw = new BufferedWriter(fw);
-            PrintWriter pw = new PrintWriter(bw);
-            String line = (maxId+1)+","+String.valueOf(userId)+","+name+","+String.valueOf(capacity)+","+String.valueOf(warning);
-            pw.println(line);
-            pw.close();
-        }
-        catch(IOException e){
-            e.printStackTrace();
-        }
-
-        return new WaterReservoir(name, capacity, warning);
-    }
-
-    private static ArrayList<WaterReservoir> getReservoirs(int ownerId){
-        File reservoirsFile = new File("./storage/reservoirs.csv");
-        ArrayList<WaterReservoir> reservoirs = new ArrayList<WaterReservoir>();
-        try{
-            FileInputStream f = new FileInputStream(reservoirsFile);
-            BufferedReader br = new BufferedReader(new InputStreamReader(f));
-            String line;
-            boolean firstLine = true;
-            while((line = br.readLine()) != null){
-                String[] splitLine = line.split(",");
-                if(firstLine){
-                    firstLine = false;
-                    continue;
-                }
-                if(Integer.parseInt(splitLine[1]) == ownerId){
-                    String name = splitLine[2];
-                    int capacity = Integer.parseInt(splitLine[3]);
-                    int warning = Integer.parseInt(splitLine[4]);
-                    reservoirs.add(new WaterReservoir(name, capacity, warning));
-                }
-            }
-            br.close();        
-        }
-        catch(FileNotFoundException e){
-            e.printStackTrace();
-        }     
-        catch(IOException e){
-            e.printStackTrace();
-        }
-        return reservoirs;
     }
 }
---
+++ StorageHandler.java
@@ -0,0 +1,194 @@
+import java.util.Scanner;
+import java.io.*; //reference: https://www.tutorialspoint.com/java/java_files_io.htm
+import java.nio.file.*;
+import java.util.ArrayList;
+
+//Referenced https://www.geeksforgeeks.org/singleton-class-java/
+
+public class StorageHandler {
+    private static StorageHandler instance = null;
+
+    private File usersFile;
+    private File reservoirsFile;
+
+    private StorageHandler(){
+        usersFile = new File("./storage/users.csv");
+        reservoirsFile = new File("./storage/reservoirs.csv");
+    }
+
+    public static StorageHandler getInstance(){
+        if(instance == null){
+            instance = new StorageHandler();
+        }
+
+        return instance;
+    }
+
+    public int registerUser(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a username: ");
+        String username = scanner.next();
+        System.out.println("Enter a password: ");
+        String password = scanner.next();
+        File usersFile = new File("./storage/users.csv");
+        //Ref: https://tutoref.com/how-to-read-and-write-files-in-java-8/
+        int maxId;
+        try{
+            FileInputStream f = new FileInputStream(usersFile);
+            BufferedReader br = new BufferedReader(new InputStreamReader(f));
+            String line;
+            maxId = 0;
+            boolean firstLine = true;
+            while((line = br.readLine()) != null){
+                String[] splitLine = line.split(",");
+                if(firstLine){
+                    firstLine = false;
+                    continue;
+                }
+                if(splitLine[1].equals(username)){
+                    System.out.println("Username already registered");
+                    return -1;
+                }
+                if(Integer.parseInt(splitLine[0]) > maxId){
+                    maxId = Integer.parseInt(splitLine[0]);
+                }
+            }
+            br.close();        
+        }
+        catch(FileNotFoundException e){
+            e.printStackTrace();
+            return -1;
+        }     
+        catch(IOException e){
+            e.printStackTrace();
+            return -1;
+        }   
+        //If the code has reached this point that username is not registered
+        try{
+            FileWriter fw = new FileWriter(usersFile, true);
+            BufferedWriter bw = new BufferedWriter(fw);
+            PrintWriter pw = new PrintWriter(bw);
+            String line = (maxId+1)+","+username+","+password;
+            pw.println(line);
+            pw.close();
+        }
+        catch(IOException e){
+            e.printStackTrace();
+            return -1;
+        }
+        return maxId+1;
+    }
+
+    public int loginUser(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a username: ");
+        String username = scanner.next();
+        System.out.println("Enter a password: ");
+        String password = scanner.next();
+        File usersFile = new File("./storage/users.csv");
+        try{
+            FileInputStream f = new FileInputStream(usersFile);
+            BufferedReader br = new BufferedReader(new InputStreamReader(f));
+            String line;
+            boolean firstLine = true;
+            while((line = br.readLine()) != null){
+                String[] splitLine = line.split(",");
+                if(firstLine){
+                    firstLine = false;
+                    continue;
+                }
+                if(splitLine[1].equals(username) && splitLine[2].equals(password)){
+                    br.close(); 
+                    return Integer.parseInt(splitLine[0]);
+                }
+            }
+            br.close();        
+        }
+        catch(FileNotFoundException e){
+            e.printStackTrace();
+            return -1;
+        }     
+        catch(IOException e){
+            e.printStackTrace();
+            return -1;
+        }
+        // This reaching this point means that username/password combo was not found
+        System.out.println("Username does not exist or incorrect password was entered");
+        return -1;
+    }
+
+    public WaterReservoir createReservoir(int userId, String name, int capacity, int warning){
+        File reservoirsFile = new File("./storage/reservoirs.csv");
+        int maxId = 0;
+        try{
+            FileInputStream f = new FileInputStream(reservoirsFile);
+            BufferedReader br = new BufferedReader(new InputStreamReader(f));
+            String line;
+            boolean firstLine = true;
+            while((line = br.readLine()) != null){
+                String[] splitLine = line.split(",");
+                if(firstLine){
+                    firstLine = false;
+                    continue;
+                }
+                if(Integer.parseInt(splitLine[0]) > maxId){
+                    maxId = Integer.parseInt(splitLine[0]);
+                }
+            }
+            br.close();        
+        }
+        catch(FileNotFoundException e){
+            e.printStackTrace();
+        }     
+        catch(IOException e){
+            e.printStackTrace();
+        }
+
+        try{
+            FileWriter fw = new FileWriter(reservoirsFile, true);
+            BufferedWriter bw = new BufferedWriter(fw);
+            PrintWriter pw = new PrintWriter(bw);
+            String line = (maxId+1)+","+String.valueOf(userId)+","+name+","+String.valueOf(capacity)+","+String.valueOf(warning);
+            pw.println(line);
+            pw.close();
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+
+        return new WaterReservoir(name, capacity, warning);
+    }
+
+    public ArrayList<WaterReservoir> getReservoirs(int ownerId){
+        File reservoirsFile = new File("./storage/reservoirs.csv");
+        ArrayList<WaterReservoir> reservoirs = new ArrayList<WaterReservoir>();
+        try{
+            FileInputStream f = new FileInputStream(reservoirsFile);
+            BufferedReader br = new BufferedReader(new InputStreamReader(f));
+            String line;
+            boolean firstLine = true;
+            while((line = br.readLine()) != null){
+                String[] splitLine = line.split(",");
+                if(firstLine){
+                    firstLine = false;
+                    continue;
+                }
+                if(Integer.parseInt(splitLine[1]) == ownerId){
+                    String name = splitLine[2];
+                    int capacity = Integer.parseInt(splitLine[3]);
+                    int warning = Integer.parseInt(splitLine[4]);
+                    reservoirs.add(new WaterReservoir(name, capacity, warning));
+                }
+            }
+            br.close();        
+        }
+        catch(FileNotFoundException e){
+            e.printStackTrace();
+        }     
+        catch(IOException e){
+            e.printStackTrace();
+        }
+        return reservoirs;
+    }
+
+}
