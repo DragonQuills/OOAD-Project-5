11c80fc0207ea64295e77a845526791f2804bde2
Kayden <kayden.adams@colorado.edu>
2020-04-21 19:43:10 -0600

    Adds vairables and constructor to timer

--- Timer.java
+++ Timer.java
@@ -2,11 +2,21 @@ class Timer{
 // attributes
   private Command light_on;
   private Command light_off;
-  private float hours_on;
+  private int hours_on;
+  private int time_left;
+  private boolean is_light_on;
 //methods
-  public void cycle(){}
+  public void hour_passed(){}
   public float get_hours_on(){return hours_on;}
   public void set_hours_on(float hours_on){}
 //constructor
-  public Timer(Command light_on, Command light_off, float hours_on){}
+  public Timer(Command new_light_on, Command new_light_off, float new_hours_on){
+    light_on = new_light_on;
+    light_off = new_light_off;
+    hours_on = new_hours_on;
+
+    light_off.execute();
+    time_left = 24 - hours_on;
+    is_light_on = false;
+  }
 }
