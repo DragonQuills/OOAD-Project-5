440b775eae4245e240538d81ebde43f693d14a2e
Ann Marie Mahon <annmarie.mahon@colorado.edu>
2020-04-25 15:19:32 -0600

    Fix errors

--- Main.java
+++ Main.java
@@ -7,6 +7,7 @@ public class Main {
         Scanner scanner = new Scanner(System.in); //For terminal input, reference: https://alvinalexander.com/java/edu/pj/pj010005/
         boolean validInput = false;
         StorageHandler storage = StorageHandler.getInstance();
+        PlantFactory factory = new PlantFactory();
         int userId = -1;
         while(!validInput){
             System.out.println("Select an option:");
@@ -63,22 +64,23 @@ public class Main {
                 System.out.println("Enter plant type");
                 String plant_type = scanner.next();
                 System.out.println("Checking our database of recommendations...");
-                PlantPot new_plant = PlantFactory.get_plant(plant_name, plant_type);
+                PlantPot new_plant = factory.get_plant(plant_name, plant_type);
                 //If data is found in query_csv(), print that data
+                String user_likes_data = "";
                 if (new_plant.get_min_temp() > 0) {
-                  System.out.println("Here's what we found:")
-                  System.out.println("Minimum Soil Humidity: ", new_plant.get_min_soil_humidity());
-                  System.out.println("Desired/Max Soil Humidity: ", new_plant.get_desired_soil_humidity());
-                  System.out.println("Light Hours: ", new_plant.get_light_hours());
-                  System.out.println("Minimum Room Temperature: ", new_plant.get_min_temp());
-                  System.out.println("Maximum Room Temperature: ", new_plant.get_max_temp());
+                  System.out.println("Here's what we found:");
+                  System.out.println("Minimum Soil Humidity: "+ new_plant.get_min_soil_humidity());
+                  System.out.println("Desired/Max Soil Humidity: "+ new_plant.get_desired_soil_humidity());
+                  System.out.println("Light Hours: "+ new_plant.get_light_hours());
+                  System.out.println("Minimum Room Temperature: "+ new_plant.get_min_temp());
+                  System.out.println("Maximum Room Temperature: "+ new_plant.get_max_temp());
                   System.out.println("Accept? (y/n)");
-                  String user_likes_data = scanner.next();
+                  user_likes_data = scanner.next();
                 }
                 //else, tell the use they must enter data
                 else {
-                  System.out.println("No data about plant type ", plant_type, ". Please enter custom data: ");
-                  String user_likes_data = "n";
+                  System.out.println("No data about plant type "+ plant_type+ ". Please enter custom data: ");
+                  user_likes_data = "n";
                 }
 
 
@@ -95,32 +97,34 @@ public class Main {
                         System.out.println("Minimum soil humidity percentage (0-100):");
                         String min_soil_humidity = scanner.next();
                         try {
-                          Float.parseFloat(min_soil_humidity);
-                          if (min_soil_humidity < 1 || min_soil_humidity > 99){
+                          Float min_soil = Float.parseFloat(min_soil_humidity);
+                          if (min_soil < 1 || min_soil > 99){
                             System.out.println("Humidity percentage must be between 0 and 100. Please try again.");
                           }
                           else{
+                            new_plant.set_min_soil_humidity(min_soil);
                             break;
                           }
                         }
                         catch(NumberFormatException e) {
-                          System.out.println("Input must be a number. Please try again:")
+                          System.out.println("Input must be a number. Please try again:");
                         }
                       }
                       while(true){
                         System.out.println("Ideal/Maximum soil humidity percentage(0-100):");
                         String desired_soil_humidity = scanner.next();
                         try {
-                          Float.parseFloat(min_soil_humidity);
-                          if (desired_soil_humidity < 1 || desired_soil_humidity > 99){
+                          Float desired_soil = Float.parseFloat(desired_soil_humidity);
+                          if (desired_soil < 1 || desired_soil > 99){
                             System.out.println("Humidity percentage must be between 0 and 100. Please try again.");
                           }
                           else{
+                            new_plant.set_desired_soil_humidity(desired_soil);
                             break;
                           }
                         }
                         catch(NumberFormatException e) {
-                          System.out.println("Input must be a number. Please try again:")
+                          System.out.println("Input must be a number. Please try again:");
                         }
                       }
                       //User Input Light Time Input Validation
@@ -128,16 +132,17 @@ public class Main {
                         System.out.println("Number of hours of light per day:");
                         String light_hours = scanner.next();
                         try {
-                          Float.parseFloat(light_hours);
-                          if (light_hours < 1 || light_hours > 24){
+                          int light = Integer.parseInt(light_hours);
+                          if (light < 1 || light > 24){
                             System.out.println("Lights must be on for 1-24 hours per day. Please try again.");
                           }
                           else{
+                            new_plant.set_light_hours(light);
                             break;
                           }
                         }
                         catch(NumberFormatException e) {
-                          System.out.println("Input must be a number. Please try again:")
+                          System.out.println("Input must be a number. Please try again:");
                         }
                       }
                       //User Input Temperature Input Validation
@@ -145,51 +150,49 @@ public class Main {
                         System.out.println("Minimum Room Temperature (Fahrenheit):");
                         String min_temp = scanner.next();
                         try {
-                          Float.parseFloat(min_temp);
-                          if (min_temp < 0 || min_temp > 120){
+                          Float temp = Float.parseFloat(min_temp);
+                          if (temp < 0 || temp > 120){
                             System.out.println("We are currently unable to provide the environment you have requested.");
                             System.out.println("Please enter a temperature between 0 and 120 degrees Fahrenheit.");
                           }
                           else{
+                            new_plant.set_min_temp(temp);
                             break;
                           }
                         }
                         catch(NumberFormatException e) {
-                          System.out.println("Input must be a number. Please try again:")
+                          System.out.println("Input must be a number. Please try again:");
                         }
                       }
                       while(true){
                         System.out.println("Maximum Room Temperature (Fahrenheit):");
                         String max_temp = scanner.next();
                         try {
-                          Float.parseFloat(max_temp);
-                          if (max_temp < 0 || max_temp > 120){
+                          Float temp = Float.parseFloat(max_temp);
+                          if (temp < 0 || temp > 120){
                             System.out.println("We are currently unable to provide the environment you have requested.");
                             System.out.println("Please enter a temperature between 0 and 120 degrees Fahrenheit.");
                           }
                           else{
+                            new_plant.set_max_temp(temp);
                             break;
                           }
                         }
                         catch(NumberFormatException e) {
-                          System.out.println("Input must be a number. Please try again:")
+                          System.out.println("Input must be a number. Please try again:");
                         }
                       }
-                      new_plant.set_min_soil_humidity(min_soil_humidity);
-                      new_plant.set_desired_soil_humidity(desired_soil_humidity);
-                      new_plant.set_light_hours(light_hours);
-                      new_plant.set_min_temp(min_temp);
-                      new_plant.set_max_temp(max_temp);
 
                     }
                   //Catch invalid input, request new input
                   //Should re-iterate at top of the "parent" while loop with new user_likes_data value
                   else {
-                        System.out.println("Invalid Input. Please type y or n and press Enter.")
+                        System.out.println("Invalid Input. Please type y or n and press Enter.");
                         user_likes_data = scanner.next();
                       }
                   }
                   //Add the plant to the selected room
+                  //TODO: Implement selected room
                   selected_room.add_plant(new_plant);
 
             }
